fdb3ddc131f068539c18f3f09fb7c05e4fc784cc
Meysam Naghavi <mysmngv@gmail.com>
2024-10-05 17:57:32 +0330

    Improve Persian translation for CLI

--- gui/cli/translations/windscribe_cli_fa.ts
+++ gui/cli/translations/windscribe_cli_fa.ts
@@ -5,7 +5,7 @@
     <name>BackendCommander</name>
     <message>
         <source>Data usage: %1 / %2</source>
-        <translation>کاربرد داده:٪ 1 /٪ 2</translation>
+        <translation>مصرف داده: %1 / %2</translation>
     </message>
     <message>
         <source>Unlimited</source>
@@ -16,15 +16,15 @@
     <name>QObject</name>
     <message>
         <source>Aborting: app did not start in time</source>
-        <translation>ساقط کردن: برنامه به موقع اغاز نشد</translation>
+        <translation>عملیات لغو شد: برنامه به موقع شروع نشد</translation>
     </message>
     <message>
         <source>Aborting: IPC communication error</source>
-        <translation>لغو: خطای ارتباطی IPC</translation>
+        <translation>عملیات لغو شد: خطای ارتباطی بین فرآیندها (IPC)</translation>
     </message>
     <message>
         <source>Not logged in</source>
-        <translation>وارد نشده</translation>
+        <translation>وارد حساب کاربری نشده‌اید</translation>
     </message>
     <message>
         <source>Firewall already on</source>
@@ -36,55 +36,55 @@
     </message>
     <message>
         <source>Firewall set to always on and can&apos;t be turned off</source>
-        <translation>دیوار اتش همیشه روشن است و نمی توان ان را خاموش کرد</translation>
+        <translation>فایروال روی حالت همیشه روشن تنظیم شده و نمی‌توان آن را خاموش کرد</translation>
     </message>
     <message>
         <source>Already logged in</source>
-        <translation>قبلا وارد سیستم شده اید</translation>
+        <translation>قبلاً وارد حساب کاربری شده‌اید</translation>
     </message>
     <message>
         <source>Already logged out</source>
-        <translation>قبلا وارد سیستم شده اید</translation>
+        <translation>قبلاً از حساب کاربری خارج شده‌اید</translation>
     </message>
     <message>
         <source>No update available</source>
-        <translation>بروزرسانی موجود نیست</translation>
+        <translation>هیچ به‌روزرسانی موجود نیست</translation>
     </message>
     <message>
         <source>Internet connectivity: %1</source>
-        <translation>اتصال اینترنت:٪ 1</translation>
+        <translation>اتصال به اینترنت: %1</translation>
     </message>
     <message>
         <source>Login state: %1</source>
-        <translation>وضعیت ورود:٪ 1</translation>
+        <translation>وضعیت ورود: %1</translation>
     </message>
     <message>
         <source>Logged out</source>
-        <translation>خارج شده</translation>
+        <translation>خارج شده‌اید</translation>
     </message>
     <message>
         <source>Logging in</source>
-        <translation>ورود به سیستم</translation>
+        <translation>در حال ورود</translation>
     </message>
     <message>
         <source>Logged in</source>
-        <translation>ورود به سیستم</translation>
+        <translation>وارد شده‌اید</translation>
     </message>
     <message>
         <source>Error: %1</source>
-        <translation>خطا:٪ 1</translation>
+        <translation>خطا: %1</translation>
     </message>
     <message>
         <source>No internet connectivity</source>
-        <translation>بدون اتصال به اینترنت</translation>
+        <translation>اتصال به اینترنت وجود ندارد</translation>
     </message>
     <message>
         <source>No API connectivity</source>
-        <translation>بدون اتصال API</translation>
+        <translation>ارتباط با API قطع است</translation>
     </message>
     <message>
         <source>Incorrect username, password, or 2FA code</source>
-        <translation>نام کاربری نادرست، رمز عبور یا کد 2FA</translation>
+        <translation>نام کاربری، رمز عبور یا کد تأیید دو مرحله‌ای اشتباه است</translation>
     </message>
     <message>
         <source>SSL error</source>
@@ -92,11 +92,11 @@
     </message>
     <message>
         <source>Session expired</source>
-        <translation>نشست منقضی شد</translation>
+        <translation>نشست منقضی شده است</translation>
     </message>
     <message>
         <source>Rate limited</source>
-        <translation>نرخ محدود</translation>
+        <translation>محدودیت استفاده اعمال شده است</translation>
     </message>
     <message>
         <source>Unknown error</source>
@@ -104,11 +104,11 @@
     </message>
     <message>
         <source>Connect state: %1</source>
-        <translation>وضعیت اتصال:٪ 1</translation>
+        <translation>وضعیت اتصال: %1</translation>
     </message>
     <message>
         <source>Connected: %1</source>
-        <translation>متصل:٪ 1</translation>
+        <translation>متصل: %1</translation>
     </message>
     <message>
         <source>Connected</source>
@@ -116,55 +116,55 @@
     </message>
     <message>
         <source>[Network interference]</source>
-        <translation>[تداخل شبکه]</translation>
+        <translation>[اختلال در شبکه]</translation>
     </message>
     <message>
         <source>Connecting: %1</source>
-        <translation>اتصال:٪ 1</translation>
+        <translation>در حال اتصال: %1</translation>
     </message>
     <message>
         <source>Connecting</source>
-        <translation>اتصال</translation>
+        <translation>در حال اتصال</translation>
     </message>
     <message>
         <source>Disconnecting</source>
-        <translation>قطع ارتباط</translation>
+        <translation>در حال قطع اتصال</translation>
     </message>
     <message>
         <source>Disconnected</source>
-        <translation>قطع</translation>
+        <translation>قطع شده</translation>
     </message>
     <message>
         <source>Error: Location does not exist or is disabled</source>
-        <translation>خطا: مکان وجود ندارد یا غیر فعال است</translation>
+        <translation>خطا: مکان مورد نظر وجود ندارد یا غیرفعال است</translation>
     </message>
     <message>
         <source>Error: You are out of data, or your account has been disabled. Upgrade to Pro to continue using Windscribe</source>
-        <translation>خطا: شما از داده ها خارج شده اید یا حساب شما غیرفعال شده است. ارتقاء به نرم افزار برای ادامه استفاده از Windscribe</translation>
+        <translation>خطا: داده شما تمام شده یا حساب کاربری شما غیرفعال شده است. برای ادامه استفاده از Windscribe به نسخه پرو ارتقا دهید</translation>
     </message>
     <message>
         <source>Error: Unable to start custom DNS service</source>
-        <translation>خطا: قادر به اغاز خدمت DNS سفارشی نیست</translation>
+        <translation>خطا: امکان شروع سرویس DNS سفارشی وجود ندارد</translation>
     </message>
     <message>
         <source>Error: WireGuard adapter setup failed</source>
-        <translation>خطا: راه اندازی اداپتور WireGuard شکست خورد</translation>
+        <translation>خطا: راه‌اندازی آداپتور WireGuard ناموفق بود</translation>
     </message>
     <message>
         <source>Error: Could not retrieve WireGuard configuration</source>
-        <translation>خطا: نتوانست پیکربندی WireGuard را بازیابی کند</translation>
+        <translation>خطا: امکان بازیابی پیکربندی WireGuard وجود ندارد</translation>
     </message>
     <message>
         <source>Unknown state</source>
-        <translation>وضعیت ناشناخته</translation>
+        <translation>وضعیت نامشخص</translation>
     </message>
     <message>
         <source>Protocol: %1:%2</source>
-        <translation>قرارداد:٪ 1:٪ 2</translation>
+        <translation>پروتکل: %1:%2</translation>
     </message>
     <message>
         <source>Firewall state: %1</source>
-        <translation>وضعیت دیواراتش:٪ 1</translation>
+        <translation>وضعیت فایروال: %1</translation>
     </message>
     <message>
         <source>Always On</source>
@@ -180,31 +180,31 @@
     </message>
     <message>
         <source>Update error: download failed. Please try again or try a different network.</source>
-        <translation>خطای بروزرسانی: بارگیری شکست خورد. لطفا دوباره تلاش کنید یا یک شبکه دیگر را امتحان کنید.</translation>
+        <translation>خطای به‌روزرسانی: دانلود ناموفق بود. لطفاً دوباره تلاش کنید یا از شبکه‌ای دیگر استفاده کنید.</translation>
     </message>
     <message>
         <source>Update error: %1</source>
-        <translation>خطای بهروزرسانی:٪ 1</translation>
+        <translation>خطای به‌روزرسانی: %1</translation>
     </message>
     <message>
         <source>Update available: %1</source>
-        <translation>بهروزرسانی موجود است:٪ 1</translation>
+        <translation>به‌روزرسانی موجود است: %1</translation>
     </message>
     <message>
         <source>available</source>
-        <translation>دسترس</translation>
+        <translation>در دسترس</translation>
     </message>
     <message>
         <source>unavailable</source>
-        <translation>دسترس نیست</translation>
+        <translation>خارج از دسترس</translation>
     </message>
     <message>
         <source>Disconnected due to reaching WireGuard key limit.  Use &quot;windscribe-cli keylimit delete&quot; if you want to delete the oldest key instead, and try again.</source>
-        <translation>به دلیل رسیدن به محدودیت کلید WireGuard قطع شده است.  اگر می خواهید قدیمی ترین کلید را حذف کنید، از &quot;windscribe-cli keylimit delete&quot; استفاده کنید و دوباره امتحان کنید.</translation>
+        <translation>به دلیل رسیدن به محدودیت کلیدهای WireGuard، اتصال قطع شد. اگر می‌خواهید قدیمی‌ترین کلید را حذف کنید و دوباره تلاش کنید، از دستور &quot;windscribe-cli keylimit delete&quot; استفاده کنید.</translation>
     </message>
     <message>
         <source>Downloading: %1%</source>
-        <translation>دانلود: ٪1٪</translation>
+        <translation>در حال دانلود: %1%</translation>
     </message>
     <message>
         <source>Username too short</source>
@@ -216,15 +216,15 @@
     </message>
     <message>
         <source>Connection is in progress.  Use &apos;windscribe-cli status&apos; to check for connection status.</source>
-        <translation>اتصال در حال انجام است.  از «وضعیت windscribe-cli» برای بررسی وضعیت اتصال استفاده کنید.</translation>
+        <translation>اتصال در حال انجام است. از دستور &apos;windscribe-cli status&apos; برای بررسی وضعیت اتصال استفاده کنید.</translation>
     </message>
     <message>
         <source>Disconnection is in progress.  Use &apos;windscribe-cli status&apos; to check for connection status.</source>
-        <translation>قطع ارتباط در حال انجام است.  از «وضعیت windscribe-cli» برای بررسی وضعیت اتصال استفاده کنید.</translation>
+        <translation>قطع اتصال در حال انجام است. از دستور &apos;windscribe-cli status&apos; برای بررسی وضعیت اتصال استفاده کنید.</translation>
     </message>
     <message>
         <source>Firewall is on.</source>
-        <translation>فایروال روشن است.</translation>
+        <translation>.فایروال روشن است.</translation>
     </message>
     <message>
         <source>Firewall is off.</source>
@@ -232,23 +232,23 @@
     </message>
     <message>
         <source>Logs sent.</source>
-        <translation>سیاهههای مربوط ارسال شد.</translation>
+        <translation>گزارش‌ها ارسال شدند.</translation>
     </message>
     <message>
         <source>Preferences reloaded.</source>
-        <translation>تنظیمات برگزیده دوباره بارگذاری شد.</translation>
+        <translation>تنظیمات دوباره بارگذاری شدند.</translation>
     </message>
     <message>
         <source>Key limit behavior is set.</source>
-        <translation>رفتار حد کلید تنظیم شده است.</translation>
+        <translation>رفتار محدودیت کلید تنظیم شده است.</translation>
     </message>
     <message>
         <source>Login is in progress.  Use &apos;windscribe-cli status&apos; to check for status.</source>
-        <translation>ورود در حال انجام است.  از «وضعیت windscribe-cli» برای بررسی وضعیت استفاده کنید.</translation>
+        <translation>ورود در حال انجام است. از دستور &apos;windscribe-cli status&apos; برای بررسی وضعیت استفاده کنید.</translation>
     </message>
     <message>
         <source>Logout is in progress.  Use &apos;windscribe-cli status&apos; to check for status.</source>
-        <translation>خروج از سیستم در حال انجام است.  از «وضعیت windscribe-cli» برای بررسی وضعیت استفاده کنید.</translation>
+        <translation>خروج در حال انجام است. از دستور &apos;windscribe-cli status&apos; برای بررسی وضعیت استفاده کنید.</translation>
     </message>
 </context>
 </TS>
